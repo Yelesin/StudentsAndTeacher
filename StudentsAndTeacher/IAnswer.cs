1907c80b8c38046041f3ce96eefcc70dc53a006f
Anton <ash2003@ukr.net>
2021-10-11 16:04:46 -0900

    <Add> ExpressionOperands, Epression classes. Fill IMemorize and IAnswer interfaces

--- StudentsAndTeacher/IAnswer.cs
+++ StudentsAndTeacher/IAnswer.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace StudentsAndTeacher
 {
+    public class ExpressionOperands
+    {
+        public int Lhs { get; }
+        public int Rhs { get; }
+
+        /// <summary>
+        /// Constructor for AnswerOperands
+        /// </summary>
+        /// <param name="lhs">Left hand side operand</param>
+        /// <param name="rhs">Right hand side operand</param>
+        public ExpressionOperands(int lhs, int rhs)
+        {
+            Lhs = lhs;
+            Rhs = rhs;
+        }
+    }
+
     interface IAnswer
     {
+        int Answer(ExpressionOperands operands);
     }
 }
--- StudentsAndTeacher/IMemorize.cs
+++ StudentsAndTeacher/IMemorize.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace StudentsAndTeacher
 {
+    public enum Operation { Plus, Minus, Multiply, Divide };
+
+    public class Expression
+    {
+        private Operation operation;
+        private ExpressionOperands operands;
+
+        const double EPS = 0.00001;
+
+        /// <summary>
+        /// Expression constructor
+        /// </summary>
+        /// <param name="op">Operation type</param>
+        /// <param name="ops">Operation operands</param>
+        public Expression(Operation op, ExpressionOperands ops)
+        {
+            operation = op;
+            operands = ops;
+        }
+
+        /// <summary>
+        /// Evaluates expression using expression type and operands
+        /// </summary>
+        /// <returns>Expression evaluated</returns>
+        public double Evaluate()
+        {
+            switch (operation)
+            {
+                case Operation.Plus:
+                    return operands.Lhs + operands.Rhs;
+
+                case Operation.Minus:
+                    return operands.Lhs - operands.Rhs;
+
+                case Operation.Multiply:
+                    return operands.Lhs * operands.Rhs;
+
+                case Operation.Divide:
+
+                    if (WithinEps(operands.Rhs, 0, EPS))
+                    {
+                        throw new ArgumentException("Error! " +
+                            "Unable to perform division operation, " +
+                            "while right hand side operand is zero!");
+                    }
+
+                    return operands.Lhs / operands.Rhs;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Checks, if two float point numbers are equal with defined epsilon.
+        /// </summary>
+        /// <param name="lhs">Left hand side operand</param>
+        /// <param name="rhs">Right hand side operand</param>
+        public static bool WithinEps(double lhs, double rhs, double eps)
+        {
+            if (Math.Abs(lhs - rhs) < eps)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
     interface IMemorize
     {
+        void Memorize(Expression expr);
     }
 }
